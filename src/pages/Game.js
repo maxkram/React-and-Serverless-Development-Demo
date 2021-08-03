efa87701abab40348524c725fb0d7fd25d1b7cb5
Maksim Kramer <max.kramer@live.com>
2021-08-03 13:29:36 -0400

    added score context. checking score in game over page to ensure that the user has played the game. added links in game over to game page and home page

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import {
   StyledGame,
   StyledScore,
@@ -6,14 +6,19 @@ import {
   StyledCharacter,
 } from "../styled/Game";
 import { Strong } from "../styled/Random";
+import { useScore } from "../contexts/ScoreContext";
 
 export default function Game({ history }) {
-  const [score, setScore] = useState(0);
-  const MAX_SECONDS = 5;
-  const [ms, setMs] = useState(0);
+  const MAX_SECONDS = 50;
+  const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+  const [score, setScore] = useScore();
+  const [currentCharacter, setCurrentCharacter] = useState("");
+  const [ms, setMs] = useState(99);
   const [seconds, setSeconds] = useState(MAX_SECONDS);
 
   useEffect(() => {
+    setRandomCharacter();
+    setScore(0);
     const currentTime = new Date();
     const interval = setInterval(() => updateTime(currentTime), 1);
     return () => clearInterval(interval);
@@ -46,21 +51,38 @@ export default function Game({ history }) {
     }
   }, [seconds, ms, history]);
 
-  const keyUpHandler = (e) => {};
+  const keyUpHandler = useCallback(
+    (e) => {
+      if (e.key === currentCharacter) {
+        setScore((prevScore) => prevScore + 1);
+      } else {
+        if (score > 0) {
+          setScore((prevScore) => prevScore - 1);
+        }
+      }
+      setRandomCharacter();
+    },
+    [currentCharacter]
+  );
 
   useEffect(() => {
     document.addEventListener("keyup", keyUpHandler);
     return () => {
       document.removeEventListener("keyup", keyUpHandler);
     };
-  }, []);
+  }, [keyUpHandler]);
+
+  const setRandomCharacter = () => {
+    const randomInt = Math.floor(Math.random() * 36);
+    setCurrentCharacter(characters[randomInt]);
+  };
 
   return (
     <StyledGame>
       <StyledScore>
         Score:<Strong>{score}</Strong>
       </StyledScore>
-      <StyledCharacter>A</StyledCharacter>
+      <StyledCharacter>{currentCharacter}</StyledCharacter>
       <StyledTimer>
         Time:{" "}
         <Strong>
